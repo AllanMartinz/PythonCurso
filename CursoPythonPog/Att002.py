edcff3c006f68575a4608e3178823f13e5643ff7
Allan Martins <allanmartinscf6@gmail.com>
2024-09-01 16:54:30 -0300

    Add Att001,  Att 002

---
+++ CursoPythonPog/Att002.py
@@ -0,0 +1,8 @@
+#var nome atribuida como "escrever no terminal"""ReadLine()"""" logo apos a string
+nome = input("Digite seu nome: ")
+#escrevea no termoinal | o f"...{...}" serve para atribuir as var dentro das strings de print """@"...{...}""""
+print(f"É um prazer em te conhecer, {nome}!")
+
+#! pode se usar o format para atribuir as chaves dentro da strings
+#print("É um prazer em te conhecer, {}!".format(nome))
+
