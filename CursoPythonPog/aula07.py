4af13029f205834f9613ec0a743156280886c3fd
Allan Martins <allanmartinscf6@gmail.com>
2024-09-06 19:22:51 -0300

    Update aula07.py

--- CursoPythonPog/aula07.py
+++ CursoPythonPog/aula07.py
@@ -20,18 +20,20 @@ print(f"A soma é {soma}, o produto é {mult} e a divisao {divi:.3f}", end=" ")
 print(f"Divisao inteira {divin} e a potencia {espo}")
 
 num = int(input("Digite um numero:"))
-ant = num - 1
-suc = num + 1
-print(f"O antecessor de {num} é {ant} e o sucessor é {suc}")
+print(f"O antecessor de {num} é {num - 1} e o sucessor é {num + 1}")
 
 nota1 = float(input("Digite qual foi a primeira nota: "))
 nota2 = float(input("Digie qual foi a segunda nota: "))
 media = (nota1 + nota2) / 2
 print(f"A media do aluno foi {media:.1f}!")
 
+firstnum = int(input("Digite um numero: "))
+print(f"O dobro: {firstnum * 2}, o triplo:{firstnum * 3} e a raiz quadrada {pow(firstnum, 0.5):.2f}")
+
+
 metros = int(input("Digite o valor de metros: "))
-cent = metros / 1000
-mili = cent / 1000
+cent = metros * 100
+mili = cent * 1000
 print(f"Em {metros} há {cent} centimetros ")
 print(f"Em {metros} há {mili} milimetros ")
 
@@ -57,16 +59,25 @@ print(f"Vc pode comprar ate US${dollar}")
 largura = float(input("Qual a lagura de sua parede em metros? "))
 altura = float(input("Qual a altura de sua parede em metros? "))
 areaQuadrada = largura * altura
-totalTinta = areaQuadrada / 2**2
+totalTinta = areaQuadrada / 2
 print(f"sera usado {totalTinta} litros para pintala ")
 
 preco = float(input("Qual o preco? "))
-desconto = preco / 0.05
+desconto = preco - (preco * 5 / 100)
 print(f"com 5% de desconto o preco fica em R$:{desconto}")
 
 salario = float(input("Qual o seu salarios? R$"))
-aumento = salario * 0.15
-print(f"Com o aumento de 15% agora o seu salario é de {aumento}! ")
+aumento = salario + (salario * 15 / 100)
+print(f"Com o aumento de 15% agora o seu salario é de {aumento:.2f}! ")
+
+c = float(input("Informe a temperatura em ºC: "))
+f = ((9 * c) / 5) + 32
+print(f"A temperatura de º{c}C é de º{f}F")
+
+km = float(input("quantos km o carro percorreu? "))
+dias = float(input("quantos dias o carro foi alugado? "))
+precoTotal = (dias * 60) + (km * 0.15)
+print(f"o preco a pagar ficou {precoTotal:.2f}")
 
 
 
